26803539c20e7df4e27c55871bdf0b07f3a27a67
Ceif Khedhiri <directeur.general@web-solution.tn>
2014-04-24 17:13:45 +0100

    Creation et intergration du Backend

---
+++ src/Pfe/Bundle/WebBundle/Controller/Frontend/PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pfe\Bundle\WebBundle\Controller\Frontend;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PageController extends Controller
+{
+    public function homeAction()
+    {
+        return $this->render('PfeWebBundle:Frontend/Page:home.html.twig');
+    }
+}
