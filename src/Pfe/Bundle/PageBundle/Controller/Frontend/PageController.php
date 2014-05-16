fd57244a21e1db8723a1b96c819e6b67990e4143
Ceif Khedhiri <directeur.general@web-solution.tn>
2014-05-16 13:59:36 +0100

    Add listener and edit template

--- src/Pfe/Bundle/PageBundle/Controller/Frontend/PageController.php
+++ src/Pfe/Bundle/PageBundle/Controller/Frontend/PageController.php
@@ -3,6 +3,7 @@
 namespace Pfe\Bundle\PageBundle\Controller\Frontend;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
@@ -16,17 +17,21 @@ class PageController extends Controller
      * Home Page entity.
      *
      */
-    public function homeAction()
+    public function homeAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
 
-        $locale = $this->container->getParameter('locale');
 
-        $entity = $em->getRepository('PfePageBundle:Page')->findHome($locale);
+        $entity = $em->getRepository('PfePageBundle:Page')->findHome($request->get('locale'));
 
-        return $this->render('PfePageBundle:Frontend/Page:home.html.twig', array(
-            'entity' => $entity,
-        ));
+        if($entity){
+            return $this->render('PfePageBundle:Frontend/Page:home.html.twig', array(
+                'entity' => $entity,
+            ));
+        }
+        else{
+            return new Response($this->get('templating')->render('PfePageBundle:Frontend/Page:404.html.twig'), 404, array('Content-Type', 'text/html'));
+        }
     }
 
     public function showAction(Request $request)
@@ -38,23 +43,28 @@ class PageController extends Controller
 
         $entity = $em->getRepository('PfePageBundle:Page')->findOneByLocaleAndSlug($locale, $slug);
 
-        if($entity->getTemplate() == 0)
-        {
-            return $this->render('PfePageBundle:Frontend/Page:default.html.twig', array(
-                'entity' => $entity,
-            ));
+        if($entity){
+            if($entity->getTemplate() == 0)
+            {
+                return $this->render('PfePageBundle:Frontend/Page:default.html.twig', array(
+                    'entity' => $entity,
+                ));
+            }
+            elseif($entity->getTemplate() == 1)
+            {
+                return $this->render('PfePageBundle:Frontend/Page:home.html.twig', array(
+                    'entity' => $entity,
+                ));
+            }
+            elseif($entity->getTemplate() == 2)
+            {
+                return $this->render('PfePageBundle:Frontend/Page:contact.html.twig', array(
+                    'entity' => $entity,
+                ));
+            }
         }
-        elseif($entity->getTemplate() == 1)
-        {
-            return $this->render('PfePageBundle:Frontend/Page:home.html.twig', array(
-                'entity' => $entity,
-            ));
-        }
-        elseif($entity->getTemplate() == 2)
-        {
-            return $this->render('PfePageBundle:Frontend/Page:contact.html.twig', array(
-                'entity' => $entity,
-            ));
+        else{
+            return new Response($this->get('templating')->render('PfePageBundle:Frontend/Page:404.html.twig'), 404, array('Content-Type', 'text/html'));
         }
     }
 }
